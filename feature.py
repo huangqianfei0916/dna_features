f6a15ed0057a49a2de65466b8fce868fca1c0357
huangqianfei <huangqianfei@tju.edu.cn>
2019-12-23 09:59:31 +0800

    Add files via upload

---
+++ feature.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+import sys
+
+from sklearn.preprocessing import MinMaxScaler
+
+sys.path.extend(["../../", "../", "./"])
+import sys, os
+import pandas as pd
+import numpy as np
+
+from binary import binary
+from DNC import DNC
+from TNC import TNC
+from ENAC import ENAC
+from NAC import NAC
+from ANF import ANF
+from CKSNAP import CKSNAP
+from PseEIIP import PseEIIP
+from EIIP import EIIP
+from RCKmer import RCKmer
+from kmer import Kmer
+from NCP import NCP
+import argparse
+
+def read_fasta(file):
+    f = open(file)
+    docs = f.readlines()
+    fasta = []
+    for seq in docs:
+        if seq.startswith(">"):
+            continue
+        else:
+            fasta.append(seq)
+
+    return np.array(fasta)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-fasta', required=True, help="fasta file name")
+    args = parser.parse_args()
+    print(args)
+
+
+    fasta = read_fasta(args.fasta)
+
+    feature_name=["binary","NCP","Kmer","DNC","TNC","ENAC","NAC","ANF","CKSNAP","PseEIIP","EIIP","RCKmer"]
+
+    feature={"binary":"binary(fasta)","NCP":"NCP(fasta)","Kmer":"Kmer(fasta)"
+        , "DNC": "DNC(fasta)","TNC":"TNC(fasta)","ENAC":"ENAC(fasta)","NAC":"NAC(fasta)",
+            "ANF":"ANF(fasta)","CKSNAP":"CKSNAP(fasta)" ,"PseEIIP":"PseEIIP(fasta)",
+             "EIIP":"EIIP(fasta)","RCKmer":"RCKmer(fasta)"}
+
+    for i in feature_name:
+        eval(feature[i])
+
+
+if __name__ == '__main__':
+    main()
